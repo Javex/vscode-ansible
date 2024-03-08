2e7ec7b34c44253fb7ff973ccc42524ddc44bbeb
Tami Takamiya <ttakamiy@redhat.com>
2024-03-07 21:08:41 -0500

    refactor

--- src/features/playbookGeneration/playbookGenerationPage.ts
+++ src/features/playbookGeneration/playbookGenerationPage.ts
@@ -1,36 +1,78 @@
 import * as vscode from "vscode";
 import { Webview, Uri } from "vscode";
+import { getNonce } from "../utils/getNonce";
+import { getUri } from "../utils/getUri";
 
-function getNonce() {
-  let text = "";
-  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+export function openNewPlaybookEditor() {
+  const options = {
+    language: "ansible"
+  };
+  const content = `---
+- name: Playbook generated!
+  hosts: all
+  become: false
+  tasks:
+    - name: Print greeting
+      ansible.builtins.debug:
+      msg: Hello Playbook Generation!
+`;
+
+return vscode.workspace.openTextDocument( {
+        language: options.language
+    } )
+    .then( doc => vscode.window.showTextDocument( doc ) )
+    .then( editor => {
+        let editBuilder = (textEdit: any) => {
+            textEdit.insert( new vscode.Position( 0, 0 ), String( content ) );
+        };
+
+        return editor.edit( editBuilder, {
+                undoStopBefore: true,
+                undoStopAfter: false
+            } )
+            .then( () => editor );
+    } );
 }
 
-function getUri(webview: Webview, extensionUri: Uri, pathList: string[]) {
-  return webview.asWebviewUri(Uri.joinPath(extensionUri, ...pathList));
+export function showPlaybookGenerationPage(extensionUri: vscode.Uri) {
+  // Create a new panel and update the HTML
+  let panel: vscode.WebviewPanel | undefined;
+  panel  = vscode.window.createWebviewPanel(
+    "noteDetailView",
+    "Title",
+    vscode.ViewColumn.One,
+    {
+      // Enable JavaScript in the webview
+      enableScripts: true,
+      localResourceRoots: [
+        vscode.Uri.joinPath(extensionUri, "out"),
+        vscode.Uri.joinPath(extensionUri, "media"),
+      ],
+      enableCommandUris: true,
+      retainContextWhenHidden: true,
+    }
+  );
+
+  panel.webview.onDidReceiveMessage((message) => {
+    const command = message.command;
+    switch (command) {
+      case "updateHtml":
+        panel!.webview.html =  getWebviewContent(panel!.webview, extensionUri, 2);
+        break;
+      case "createPlaybook":
+        openNewPlaybookEditor();
+        break
+    }
+  });
+
+  panel.title = "Playbook Generation";
+  panel.webview.html = getWebviewContent(panel.webview, extensionUri);
 }
 
 export function getWebviewContent(webview: Webview, extensionUri: Uri, index=1) {
   const webviewUri = getUri(webview, extensionUri, ["out", "client", "webview", "apps", "playbookGeneration", "main.js"]);
   const styleUri = getUri(webview, extensionUri, ["media", "playbookGeneration", "style.css"]);
-
   const nonce = getNonce();
-
-  webview.onDidReceiveMessage((message) => {
-    const command = message.command;
-    switch (command) {
-      case "requestNoteData":
-        webview.postMessage({
-          command: "receiveDataInWebview",
-          payload: "",
-        });
-        break;
-    }
-  });
 
   const html1 = /*html*/ `
   <!DOCTYPE html>
@@ -91,74 +133,4 @@ export function getWebviewContent(webview: Webview, extensionUri: Uri, index=1) 
   `;
 
   return index === 1 ? html1 : html2;
-}
-
-export function openNewPlaybookEditor() {
-  const options = {
-    language: "ansible"
-  };
-  const content = `---
-- name: Playbook generated!
-  hosts: all
-  become: false
-  tasks:
-    - name: Print greeting
-      ansible.builtins.debug:
-      msg: Hello Playbook Generation!
-`;
-
-return vscode.workspace.openTextDocument( {
-        language: options.language
-    } )
-    .then( doc => vscode.window.showTextDocument( doc ) )
-    .then( editor => {
-        let editBuilder = (textEdit: any) => {
-            textEdit.insert( new vscode.Position( 0, 0 ), String( content ) );
-        };
-
-        return editor.edit( editBuilder, {
-                undoStopBefore: true,
-                undoStopAfter: false
-            } )
-            .then( () => editor );
-    } );
-}
-
-export function showPlaybookGenerationPage(extensionUri: vscode.Uri) {
-  // Create a new panel and update the HTML
-  let panel: vscode.WebviewPanel | undefined;
-  panel  = vscode.window.createWebviewPanel(
-    "noteDetailView",
-    "Title",
-    vscode.ViewColumn.One,
-    {
-      // Enable JavaScript in the webview
-      enableScripts: true,
-      localResourceRoots: [
-        vscode.Uri.joinPath(extensionUri, "out"),
-        vscode.Uri.joinPath(extensionUri, "media"),
-      ],
-      enableCommandUris: true,
-      retainContextWhenHidden: true,
-      // Restrict the webview to only load resources from the `out` directory
-      // localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, "out")],
-    }
-  );
-
-  // If a panel is open, update the HTML with the selected item's content
-  // If a panel is open and receives an update message, update the notes array and the panel title/html
-  panel.webview.onDidReceiveMessage((message) => {
-    const command = message.command;
-    switch (command) {
-      case "updateHtml":
-        panel!.webview.html =  getWebviewContent(panel!.webview, extensionUri, 2);
-        break;
-      case "createPlaybook":
-        openNewPlaybookEditor();
-        break
-    }
-  });
-
-  panel.title = "Playbook Generation";
-  panel.webview.html = getWebviewContent(panel.webview, extensionUri);
 }
